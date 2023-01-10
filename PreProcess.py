8351bee107e3185e5c858ec1778a68130df3b9af
Tony Brown <tbrown42@dxc.com>
2023-01-10 14:52:56 +0000

    Added Cooled Camera Processing and Output of Stats

--- PreProcess.py
+++ PreProcess.py
@@ -9,6 +9,7 @@ from pysiril.addons import *
 def set_defaults():
     global arg_workingdir
     global arg_masterbiasfile
+    global arg_masterdarkfile
     global arg_sequencestart
     global arg_targetname
     global arg_process
@@ -16,6 +17,7 @@ def set_defaults():
     global arg_filterround
     global arg_filterwfwhm
     global arg_cpus
+    global arg_camera
 
     # Set default args
     arg_workingdir = os.getcwd()
@@ -23,23 +25,25 @@ def set_defaults():
     arg_masterbiasfile = ""
     arg_targetname = ""
     arg_process = "stack"
+    arg_cpus = 4
+    arg_camera = "CANON_DSLR"
+
+    # Default filters used that in effect turn off filtering
     arg_filterfwhm = 10
     arg_filterround = 0.1
     arg_filterwfwhm = 30
-    arg_cpus = 4
-
-
+    
 def init(argv):
-    arg_help = "{0} -d <base directory> -s <sequence start number> -b <master bias location> -t <target name> -p <process (reg, stack, preproc)) -f <Filter FWHM> -w <Filter wFWHM> -r <Filter Roundness (0-1)> ".format(argv[0])
+    arg_help = "{0} -i <Camera (CANON_DSLR / ZWO_OSC)> -d <base directory> -s <sequence start number> -a <master dark file> -b <master bias(offset) file> -t <target name> -p <process (reg, stack, preproc)) -f <Filter FWHM> -w <Filter wFWHM> -r <Filter Roundness (0-1)> ".format(argv[0])
     
     # Set the defaults
     set_defaults()
 
     try:
-        opts, args = getopt.getopt(argv[1:], "hd:s:b:t:p:f:w:r:c:", ["help", "basedir=", 
-        "sequencestart=", "masterbiasfile=", "targetname=", "process=", 
+        opts, args = getopt.getopt(argv[1:], "hi:d:s:a:b:t:p:f:w:r:c:", ["help", "camera=", "basedir=", 
+        "sequencestart=", "masterdarkfile=", "masterbiasfile=", "targetname=", "process=", 
         "filterfwhm=", "filterwfwhm=", "filterround=", "cpu=" ])
-        # Minimum number of arguments are bias location and target name - all others can de defaulted
+        # Minimum number of arguments are bias or dark location and target name - all others can de defaulted
         if len(opts)<2:
             print(arg_help)  # print the help message
             sys.exit(3)
@@ -51,12 +55,18 @@ def init(argv):
         if opt in ("-h", "--help"):
             print(arg_help)  # print the help message
             sys.exit(2)
+        if opt in ("-i", "--camera"):
+            global arg_camera
+            arg_camera = arg.upper()
         elif opt in ("-d", "--basedir"):
             global arg_workingdir
             arg_workingdir = arg
         elif opt in ("-s", "--sequencestart"):
             global arg_sequencestart
             arg_sequencestart = arg
+        elif opt in ("-a", "--masterdarkfile"):
+            global arg_masterdarkfile
+            arg_masterdarkfile = arg
         elif opt in ("-b", "--masterbiasfile"):
             global arg_masterbiasfile
             arg_masterbiasfile = arg
@@ -87,18 +97,36 @@ def init(argv):
             global arg_cpus
             arg_cpus=arg
     
-
-def count_files(dir):
+# Counts files in a given directory, camera is passed in for extension selection
+def count_files(camera,dir):
     fct=Addons(app)
-    return fct.GetNbFiles(os.path.join(dir, '*.cr2'))
+    if (camera=="CANON_DSLR"):
+        return fct.GetNbFiles(os.path.join(dir, '*.cr2'))
+    elif (camera=="ZWO_OSC"):
+        return fct.GetNbFiles(os.path.join(dir, '*.fit')) + fct.GetNbFiles(os.path.join(dir, '*.fits'))
+
+# NOTE - Sigma low and High are set here for Offset file (Bias for DSLR / DarkFlat for ZWO)
+def master_offset(camera, offset_dir, seq_start, process_dir):
+    cmd.cd(offset_dir)
+    if(camera=="ZWO_OSC"):
+        cmd.convert( 'offset', out=process_dir, start=seq_start )
+    else:
+        cmd.convertraw( 'offset', out=process_dir, start=seq_start )
+    cmd.cd( process_dir )
+    cmd.seqstat('offset','offset.csv', option='main')
+    cmd.stack( 'offset', type='rej', sigma_low=3.0, sigma_high=3.0, out='master_offset.fit' )
 
 # NOTE - Sigma low and High are set here for Flats
-def master_flat(flat_dir, bias_file, seq_start, process_dir):
+def master_flat(camera, flat_dir, offset_file, seq_start, process_dir):
     cmd.cd(flat_dir)
-    cmd.convertraw( 'flat', out=process_dir, start=seq_start )
+    if(camera=="ZWO_OSC"):
+        cmd.convert( 'flat', out=process_dir, start=seq_start )
+    else:
+        cmd.convertraw( 'flat', out=process_dir, start=seq_start )
     cmd.cd( process_dir )
-    cmd.preprocess( 'flat', bias=bias_file )
-    cmd.stack( 'pp_flat', type='rej', sigma_low=3.0, sigma_high=3.0, norm='mul')
+    cmd.preprocess( 'flat', bias=offset_file )
+    cmd.seqstat('pp_flat','pp_flat.csv', option='main')
+    cmd.stack( 'pp_flat', type='rej', sigma_low=3.0, sigma_high=3.0, norm='mul', out='master_flat.fit')
 
 # NOTE - Sigma low and High are set here for Darks
 def master_dark(dark_dir, bias_file, seq_start, process_dir):
@@ -106,23 +134,37 @@ def master_dark(dark_dir, bias_file, seq_start, process_dir):
     cmd.convertraw( 'dark', out=process_dir, start=seq_start )
     cmd.cd( process_dir )
     cmd.preprocess( 'dark', bias=bias_file )
-    cmd.stack( 'pp_dark', type='rej', sigma_low=3.5, sigma_high=3.5, norm='no')
+    cmd.seqstat('pp_dark','pp_dark.csv', option='main')
+    cmd.stack( 'pp_dark', type='rej', sigma_low=3.5, sigma_high=3.5, norm='no', out='master_dark.fit')
 
-def pre_process_lights(light_dir, bias_file, seq_start, process_dir):
+def pre_process_lights(camera, light_dir, bias_file, dark_file, seq_start, process_dir):
     cmd.cd(light_dir)
-    cmd.convertraw( 'light', out=process_dir, start=seq_start )
+    if(camera=="ZWO_OSC"):
+        cmd.convert( 'light', out=process_dir, start=seq_start )
+    else:
+        cmd.convertraw( 'light', out=process_dir, start=seq_start )
     cmd.cd( process_dir )
-    # Dark Optimised with hot pixel cosmetic correction of sigma 3, no cold.
-    cmd.preprocess( 'light', bias=bias_file, flat='pp_flat_stacked', 
-                    dark='pp_dark_stacked', debayer=True, 
-                    opt=True, cfa=True, equalize_cfa=True)
+    cmd.seqstat('light','light.csv', option='main')
+
+    if(camera=="ZWO_OSC"):
+        # No Dark optimisation as cooled camera will expect to use Dark at same temperature, therefore no bias (Offset) specified here - it is part of pp_flats
+        cmd.preprocess( 'light', flat='master_flat.fit', 
+                        dark=dark_file, debayer=True, 
+                        cfa=True, equalize_cfa=True, prefix='pp_')
+    else:
+        # Dark Optimised which requires the bias(Offset) , dark and Flat
+        cmd.preprocess( 'light', bias=bias_file, flat='master_flat.fit', 
+                        dark=dark_file, debayer=True, 
+                        opt=True, cfa=True, equalize_cfa=True, prefix='pp_')
+    cmd.seqstat('pp_light','pp_light.csv', option='main')
 
 # Need to set a reference image as default will select the first, if this is rubbish (no or few stars)
 # then fails.
 # TO DO - when 2pass is provided then run as it is possible if the first image is poor then reference
 # for the registration will be poor
 def register_lights():    
-    cmd.register( 'pp_light' )
+    cmd.register( 'pp_light',prefix='r_' )
+    cmd.seqstat('r_pp_light','r_pp_light.csv', option='main')
 
 ## Stack using filters to attempt to throw away bad frames.
 ## Roundness, FWHM and wFWHM are all used.
@@ -162,6 +204,7 @@ def process():
         processDir=os.path.join(arg_workingdir, 'process')
         sourceFlatDir=os.path.join(arg_workingdir, 'FLAT')
         sourceDarkDir=os.path.join(arg_workingdir, 'DARK')
+        sourceOffsetDir=os.path.join(arg_workingdir, 'DARKFLAT')
         sourceLightDir=os.path.join(arg_workingdir,'LIGHT', arg_targetname)
      
         indexStart=int(arg_sequencestart)
@@ -170,9 +213,17 @@ def process():
         print('*****************************************')
         print('  DIRECTORIES: -')
         print('    Working Directory = ' + arg_workingdir)
-        print('    Bias Master File = ' + arg_masterbiasfile)
+        print('    Processing for camera = ' + arg_camera)
+        if arg_camera=="CANON_DSLR":
+            global arg_masterbiasfile
+            print('    Bias Master File = ' + arg_masterbiasfile)      
+        if arg_camera=="ZWO_OSC":    
+            global arg_masterdarkfile
+            print('    Dark Master File = ' + arg_masterdarkfile)
+
         print('    Light Directory = ' + sourceLightDir)
         print('    Flat Directory = ' + sourceFlatDir)
+        print('    Offset (DarkFlats/Bias) Directory = ' + sourceOffsetDir)
         print('    Dark Directory = ' + sourceDarkDir)
         print('    Process Directory = ' + processDir)
         print('\n  Options: -')
@@ -186,18 +237,26 @@ def process():
         print('*****************************************')
 
         # Prepare Master frames
-        fileCount = count_files(sourceFlatDir)
+        if arg_camera=="ZWO_OSC":
+            fileCount = count_files(arg_camera, sourceOffsetDir)
+            print ('Processing ' + str(fileCount) + ' offset')
+            master_offset(arg_camera ,sourceOffsetDir, indexStart, processDir)
+            arg_masterbiasfile = 'master_offset.fit'
+
+        fileCount = count_files(arg_camera, sourceFlatDir)
         print ('Processing ' + str(fileCount) + ' flats')
-        master_flat(sourceFlatDir, arg_masterbiasfile, indexStart, processDir)
+        master_flat(arg_camera ,sourceFlatDir, arg_masterbiasfile, indexStart, processDir)
         
-        fileCount = count_files(sourceDarkDir)
-        print ('Processing ' + str(fileCount) + ' darks')
-        master_dark(sourceDarkDir, arg_masterbiasfile, indexStart, processDir)
+        if arg_camera=="CANON_DSLR":
+            fileCount = count_files(arg_camera, sourceDarkDir)
+            print ('Processing ' + str(fileCount) + ' darks')
+            master_dark(sourceDarkDir, arg_masterbiasfile, indexStart, processDir)
+            arg_masterdarkfile='master_dark.fit'
         
         # Dark optimisation and debayering is performed here
-        fileCount = count_files(sourceLightDir)
+        fileCount = count_files(arg_camera, sourceLightDir)
         print ('Processing ' + str(fileCount) + ' lights')
-        pre_process_lights(sourceLightDir, arg_masterbiasfile, indexStart, processDir)
+        pre_process_lights(arg_camera, sourceLightDir, arg_masterbiasfile, arg_masterdarkfile, indexStart, processDir)
 
         # If requested automatically stack all registered Lights
         if arg_process in ("stack", "reg"):
--- README.md
+++ README.md
@@ -1,23 +1,43 @@
 # sirilAutomation
 Automated processing of Astronomy images using Siril
 
-## Command Line Execution
+## Command Line Execution for DSLR File Processing
 Simply run the PreProcess.py file with parameters:-
 
-python3 PreProcess.py -b <Master bias file path> -t <Target location name> -p <stage> -f <filter FWHM> -w <filter wFWHM> -r <filter Roundness> -d <base directory> -s <Start Index>
+python3 PreProcess.py -i CANON_DSLR --b <Master bias file path> -t <Target location name> -p <stage> -f <filter FWHM> -w <filter wFWHM> -r <filter Roundness> -d <base directory> -s <Start Index>
 
-The assumption when running the command is that there will be a directory layout as follows: -
+The assumption when running the command is that there will be a directory layout as follows if using a DSLR where there are Dark files taken and a master Bias: -
 
 ![Expected directory organisation](./images/ExampleFileStructure.png)
 
 Folders expect to contain *.cr2 raw cannon files.
 The 'process' directory is expected to have already been created, any files in this directory with matching names during processing will be overwritten.
+
+## Command Line Execution for ZWO OSC File Processing
+Simply run the PreProcess.py file with parameters:-
+
+python3 PreProcess.py -i ZWO_OSC -a <Master dark file path> -t <Target location name> -p <stage> -f <filter FWHM> -w <filter wFWHM> -r <filter Roundness> -d <base directory> -s <Start Index>
+
+The assumption when running the command is that there will be a directory layout as follows if using a ZWO OSC camera where there is a Master Dark file already available and your have taken DarkFlats to act as Bias (Offset files): -
+
+![Expected directory organisation](./images/ExampleZWOOSCFileStructure.png)
     
+Note - although the -i (camera) parameter is set to ZWO_OSC this should work with any modern CMOS colour camera with FITS files as output
+
+### Camera Type -i
+Optional - will default to CANON_DSLR if not specified.
+Currently CANON_DSLR and ZWO_OSC are the only valid options.  
+When DSLR is specified the script expects that there is a master Bias to be used and that the session will have Dark images that must be stacked.  Also, as the camera is not cooled the pre-processing of lights is performed using the dark optimisation option.
+When ZWO_OSC is specified the script expects that there is a master Dark and that the session will have DarkFlats that will act as the Bias for pre-processing the Flats.  Dark optimisation is also not used as part of the lights pre-processing.
+
 ### Base Directory -d
 Optional - if no base directory is provided then the current working directory is assumed to be the directory otherwise provide the path to the base directory.  In the image above, the path to Siril_Test would be the base directory.
 
+### Master dark file path -a
+Required if Camera = "ZWO_OSC" - path and name of the master dark file to use to use for the pre-processing of Lights
+
 ### Master bias file path -b
-Required - path and name of the master bias file to use to use for the pre-processing of files
+Required if Camera = "CANON_DSLR"- path and name of the master Offset file to use to use for the pre-processing of files
 
 ### Target location name -t
 Required - Source light files are expecting to be in a folder that corresponds to the target.  In the image above, the target would be IC\ 1848 - '\\' encodes the space character.
